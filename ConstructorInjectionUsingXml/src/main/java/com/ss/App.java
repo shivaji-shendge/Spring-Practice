f562985097cdb1f7173c120fc67a6fea9127cc1c
Shivaji Shendge <shivajishendge2001@gmail.com>
2025-02-12 22:28:36 +0530

    constructor injection using xml  added

---
+++ ConstructorInjectionUsingXml/src/main/java/com/ss/App.java
@@ -0,0 +1,16 @@
+package com.ss;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+		Person p = (Person) context.getBean("person");
+		System.out.println(p);
+
+	}
+
+}
